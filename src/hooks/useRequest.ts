5e3288d2d1ddb5404e1604673668317cf3d162d2
Luis Henrique <luishenriquedesouza112@gmail.com>
2022-10-29 21:08:49 -0300

    change: option to new request using effect

--- src/hooks/useRequest.ts
+++ src/hooks/useRequest.ts
@@ -1,12 +1,12 @@
 import { useEffect, useState } from "react";
 
-export function useRequest<T = unknown>(request: string) {
+export function useRequest<T = unknown>(request: string, onChange?: any) {
   const [data, setData] = useState<T | null>(null);
   useEffect(() => {
     fetch(request)
       .then((response) => response.json())
       .then((data) => setData(data));
-  }, []);
+  }, [onChange]);
 
-  return { data };
+  return data;
 }
